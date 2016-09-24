b5ce3952820a799ddf78cfcdcd8f906c3b368a89
raffaeler <raffaeler@vevy.com>
2016-09-24 11:20:20 +0200

    Assembly attributes

--- NugetInstall/Properties/AssemblyInfo.cs
+++ NugetInstall/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NugetInstall")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Helps automating the nuget publishing process")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Raffaele Rialdi")]
 [assembly: AssemblyProduct("NugetInstall")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © Raffaele Rialdi 2016, @raffaeler")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
